c979a1c02e03937d43c4c3401f74085f1b4b1084
Japs Asuncion <asuncionjaps@gmail.com>
2019-06-26 17:40:36 +1000

    add clear fields and handler for export response (#4)
    add clear fields and handler for export response

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -150,7 +150,7 @@ export class UserComponent implements OnInit {
         if (!this.settings.deals.length) {
             return;
         }
-        this.vrApi.exportDeals(this.settings);
+        this.vrApi.exportDeals(this.settings).subscribe((b) => this.handleExportResponse(b));;
     }
 
     selectDeal(deal: IInstrumentSettings) {
@@ -175,6 +175,35 @@ export class UserComponent implements OnInit {
                     return { isChecked: false, instrumentType: deal.instrumentType, fields: [] } as IInstrumentSettings;
                 })
             });
+    }
+
+    handleExportResponse(blob: Blob): void {
+        // It is necessary to create a new blob object with mime-type explicitly set
+        // otherwise only Chrome works like it should
+        const newBlob = new Blob([blob], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+
+        // IE doesn't allow using a blob object directly as link href
+        // instead it is necessary to use msSaveOrOpenBlob
+        if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+            window.navigator.msSaveOrOpenBlob(newBlob);
+            return;
+        }
+
+        // For other browsers: 
+        // Create a link pointing to the ObjectURL containing the blob.
+        const data = window.URL.createObjectURL(newBlob);
+
+        const link = document.createElement('a');
+        link.href = data;
+        link.download = 'instruments.xls';
+        // this is necessary as link.click() does not work on the latest firefox
+        link.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true, view: window }));
+
+        setTimeout(() => {
+            // For Firefox it is necessary to delay revoking the ObjectURL
+            window.URL.revokeObjectURL(data);
+            link.remove();
+        }, 100);
     }
 }
 
