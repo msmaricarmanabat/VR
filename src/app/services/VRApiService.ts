15babf04351c16357dca95b6a101e6a3724e04f0
Japs Asuncion <Johna@visualrisk.com>
2019-06-26 15:14:51 +1000

    add initial service call for export deals

--- src/app/services/VRApiService.ts
+++ src/app/services/VRApiService.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthenticationService } from './AuthenticationService';
 import { Subscription, Observable } from 'rxjs';
+import { ICompareSettings } from '../models/CompareSettings';
 
 @Injectable({ providedIn: 'root' })
 export class VRApiService {
@@ -9,8 +10,14 @@ export class VRApiService {
 
     getFields(): Observable<Object> {
         const headers = this.getDefaultHeader();
-        const url = `${this.getBaseUrl()}/api/comparetool/getfields`;
+        const url = `${this.getBaseUrl()}/api/reconciliationtool/getfields`;
         return this.http.get(url, { headers });
+    }
+
+    exportDeals(settings: ICompareSettings):void{
+        const headers = this.getDefaultHeader();
+        const url = `${this.getBaseUrl()}/api/reconciliationtool/getexcelfile`;
+        this.http.post(url, settings, { headers });
     }
 
     private getDefaultHeader(): HttpHeaders {
